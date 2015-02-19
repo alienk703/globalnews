0b8813c0831e48ffffd79dc04a25aa081c388328
alienk703 <alienk703@gmail.com>
2015-02-18 22:55:31 -0500

    initial

---
+++ javascript/bing.js
@@ -0,0 +1,5 @@
+
+
+
+// http://api.bing.net/json.aspx?AppId=yP8CJ0pd4Ya1sZoQdCu6DTktubpKSPi8ppXp0tIknlQ&Query=msn%20moneycentral&Sources=News&Version=2.0&Market=en-us&Options=EnableHighlighting&News.Offset=0&News.SortBy=Relevance&JsonType=callback&JsonCallback=SearchCompleted
+
